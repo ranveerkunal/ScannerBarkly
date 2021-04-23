a228dabe12e49242e67f1308c477601ddb83f19c
Ranveer Kunal <ranveerkunal@gmail.com>
2021-04-23 08:35:35 -0700

    build

--- portal/build.sh
+++ portal/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 flutter build web --release
+cp ../py/collage/qr.jpg assets/data/
 cp ../py/config.json assets/data/
 cp -rf assets/data/* ../docs/assets/data/
 cp -rf build/web/* ../docs
