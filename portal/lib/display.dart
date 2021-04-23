32b0e6918d9fcb8ea2c04654a50285f78b9f4b6f
Ranveer Kunal <ranveerkunal@gmail.com>
2021-04-23 14:11:56 -0700

    build

--- portal/lib/display.dart
+++ portal/lib/display.dart
@@ -4,6 +4,7 @@ import 'package:portal/selector.dart';
 import 'package:provider/provider.dart';
 
 class Display extends StatelessWidget {
+  final ValueNotifier<bool> showQr = ValueNotifier<bool>(false);
   final double size;
 
   Display({Key? key, required this.size}) : super(key: key);
@@ -15,33 +16,48 @@ class Display extends StatelessWidget {
     final asset = context.watch<Map<int, TileAsset>>()[rank]!;
     if (asset.img == null) return Container();
     final scale = context.read<double>();
-    return Container(
-      alignment: Alignment.center,
-      height: size,
-      width: size,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 40 * scale,
-            alignment: Alignment.center,
-            child: Text('${asset.config.name} #${asset.config.rank}'),
-          ),
-          AnimatedSwitcher(
-            duration: Duration(seconds: 3),
-            child: Image(
-              image: asset.img!,
-              gaplessPlayback: true,
-              fit: BoxFit.cover,
-              height: 420 * scale,
-              width: 420 * scale,
+    return ChangeNotifierProvider<ValueNotifier<bool>>.value(
+      value: showQr,
+      child: Container(
+        alignment: Alignment.center,
+        height: size,
+        width: size,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 40 * scale,
+              alignment: Alignment.center,
+              child: Text('${asset.config.name} #${asset.config.rank}'),
             ),
-          ),
-          Container(
-            height: 40 * scale,
-            alignment: Alignment.center,
-          ),
-        ],
+            Builder(
+              builder: (BuildContext context) {
+                return AnimatedSwitcher(
+                  duration: Duration(seconds: 3),
+                  child: context.watch<ValueNotifier<bool>>().value
+                      ? Image(
+                          image: AssetImage('data/qr.jpg'),
+                          gaplessPlayback: true,
+                          fit: BoxFit.cover,
+                          height: 420 * scale,
+                          width: 420 * scale,
+                        )
+                      : Image(
+                          image: asset.img!,
+                          gaplessPlayback: true,
+                          fit: BoxFit.cover,
+                          height: 420 * scale,
+                          width: 420 * scale,
+                        ),
+                );
+              },
+            ),
+            Container(
+              height: 40 * scale,
+              alignment: Alignment.center,
+            ),
+          ],
+        ),
       ),
     );
   }
